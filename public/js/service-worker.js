8d82d917c99687a1a18afdf5960477a3013ef33c
John-et <tripham178@gmail.com>
2019-03-12 12:52:02 +0700

    service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,3 +1,9 @@
 self.addEventListener("install", e => {
-  console.log("[ServiceWorker]");
+  console.log("[ServiceWorker] Installed");
+});
+self.addEventListener("active", e => {
+  console.log("[ServiceWorker] Activated");
+});
+self.addEventListener("fetch", e => {
+  console.log("[ServiceWorker] Fetching", e.request.url);
 });

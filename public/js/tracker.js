2012adf1a9ba57183fcce3bab1812acd9f1728dd
John-et <tripham178@gmail.com>
2019-03-13 10:26:30 +0700

    testing

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -1,51 +1,51 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const socket = io("/");
-//   const positionOptions = {
-//     enableHighAccuracy: true,
-//     maximumAge: 0
-//   };
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io("/");
+  const positionOptions = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  };
 
-//   setInterval(() => {
-//     console.log("tick");
-//     navigator.geolocation.getCurrentPosition(
-//       pos => {
-//         const { latitude: lat, longitude: lng } = pos.coords;
-//         socket.emit("updateLocation", { lat, lng });
-//       },
-//       err => {
-//         console.error(err);
-//       },
-//       positionOptions
-//     );
-//   }, 2000);
-// });
+  setInterval(() => {
+    console.log("tick");
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        const { latitude: lat, longitude: lng } = pos.coords;
+        socket.emit("updateLocation", { lat, lng });
+      },
+      err => {
+        console.error(err);
+      },
+      positionOptions
+    );
+  }, 2000);
+});
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("js/service-worker.js")
-    .then(registration => {
-      console.log("service worker Registered", registration);
-      const socket = io("/");
-      const positionOptions = {
-        enableHighAccuracy: true,
-        maximumAge: 0
-      };
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("js/service-worker.js")
+//     .then(registration => {
+//       console.log("service worker Registered", registration);
+//       const socket = io("/");
+//       const positionOptions = {
+//         enableHighAccuracy: true,
+//         maximumAge: 0
+//       };
 
-      setInterval(() => {
-        console.log("tick");
-        navigator.geolocation.getCurrentPosition(
-          pos => {
-            const { latitude: lat, longitude: lng } = pos.coords;
-            socket.emit("updateLocation", { lat, lng });
-          },
-          err => {
-            console.error(err);
-          },
-          positionOptions
-        );
-      }, 2000);
-    })
-    .catch(err => {
-      console.log("Service Worker Failed to Register", err);
-    });
-}
+//       setInterval(() => {
+//         console.log("tick");
+//         navigator.geolocation.getCurrentPosition(
+//           pos => {
+//             const { latitude: lat, longitude: lng } = pos.coords;
+//             socket.emit("updateLocation", { lat, lng });
+//           },
+//           err => {
+//             console.error(err);
+//           },
+//           positionOptions
+//         );
+//       }, 2000);
+//     })
+//     .catch(err => {
+//       console.log("Service Worker Failed to Register", err);
+//     });
+// }
